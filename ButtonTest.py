692e9f5df49f9449d73ee2742fef33ddebbc75ea
dangormanCPS <69629474+dangormanCPS@users.noreply.github.com>
2024-03-28 17:02:44 +0000

    Update ButtonTest.py

--- ButtonTest.py
+++ ButtonTest.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.header('st.button')
+st.header('This is a test to show a button (st.button) in action')
 
 st.button("Reset", type="primary")
 if st.button('Say hello to Streamlit app :smile:'):
