1f2a0772a5a06c48cc999b26cddd84c792a2859a
dangormanCPS <69629474+dangormanCPS@users.noreply.github.com>
2024-04-05 14:38:49 +0100

    Update Charts.py

--- Charts.py
+++ Charts.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header('Dan did a thing. Its just a Line chart but its coded from scratch')
+st.header('Dan did a thing. And Meg was amazed. Its just a Line chart but its coded from scratch')
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 4),
